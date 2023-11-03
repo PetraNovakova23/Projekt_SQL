edc28059b630e5138f111ca5349c322a23475528
Petra Nováková <peta.tm@seznam.cz>
2023-11-03 12:49:20 +0100

    Upravila jsem v otázce 4 a 5 průměrné ceny potravin, abych získala průměr za jednotlivé roky a mohla porovnat se mzdami a HDP bez jednotlivých odvětvích. Zároveň jsem dokončila Průvodní listinu, kde jsem doplnila vizualizaci o otázky 4 a 5 a popsala výsledky. Výsledný soubor jsem uložila jako PDF.

--- Otázka 4.sql
+++ Otázka 4.sql
@@ -1,39 +1,33 @@
-CREATE VIEW v_average_wages AS
-SELECT
-	AVG(value) AS avg_wages,
-	payroll_year 
+CREATE VIEW v_average_year AS
+SELECT 
+	AVG(value) AS avg_wages_year,
+    AVG(price) AS avg_price_year,
+    payroll_year  
 FROM t_petra_novakova_project_sql_primary_final tpnpspf 
-WHERE 
-	value_type_code = 5958 AND 
-	value IS NOT NULL 
+WHERE price IS NOT NULL 
+    AND value IS NOT NULL
 GROUP BY payroll_year ;
 
-CREATE VIEW v_average_end_wages AS
-SELECT
-	AVG(value) AS avg_end_wages,
+CREATE VIEW v_average_end_year AS
+SELECT 
+	AVG(value) AS avg_wages_end_year,
+	AVG(price) AS avg_price_end_year,
 	payroll_year + 1 AS end_year
 FROM t_petra_novakova_project_sql_primary_final tpnpspf 
-WHERE 
-	value_type_code = 5958 AND 
+WHERE price IS NOT NULL AND 
 	value IS NOT NULL 
-GROUP BY end_year ;
+GROUP BY end_year;
 
 SELECT 
-	vap.payroll_year, 
-	vaep.end_year, 
-	(avg_wages - avg_end_wages)/avg_end_wages * 100 AS yearonyear_increase_wages,
-	(avg_price - avg_end_price)/avg_end_price * 100 AS yearonyear_increase_price,
+	vay.payroll_year, 
+	vaey.end_year, 
+	(avg_wages_year - avg_wages_end_year)/avg_wages_end_year * 100 AS yearonyear_increase_wages,
+	(avg_price_year - avg_price_end_year)/avg_price_end_year * 100 AS yearonyear_increase_price,
 	CASE 
-		WHEN (avg_price - avg_end_price)/avg_end_price * 100 - (avg_wages - avg_end_wages)/avg_end_wages * 100 >10 THEN 'up 10%'
+		WHEN (avg_price_year - avg_Price_end_year)/avg_price_end_year * 100 - (avg_wages_year - avg_wages_end_year)/avg_wages_end_year * 100 >10 THEN 'up 10%'
 		ELSE 'under 10%'
 	END AS change_percent
-FROM v_average_price vap 
-JOIN v_average_end_price vaep  
-	ON vaep.end_year = vap.payroll_year 
-JOIN v_average_wages vaw 
-	ON vaw.payroll_year = vap.payroll_year 
-JOIN v_average_end_wages vaew 
-	ON vaew.end_year = vap.payroll_year ;
-
-
+FROM v_average_year vay 
+JOIN v_average_end_year vaey 
+	ON vaey.end_year = vay.payroll_year;
 	
--- Otázka 5.sql
+++ Otázka 5.sql
@@ -11,26 +11,20 @@ SELECT
 FROM t_petra_novakova_project_sql_secondary_final tpnpssf 
 WHERE country = 'Czech Republic';
 
-DROP VIEW v_end_gdp ;
 
 SELECT 
-	vap.payroll_year,
-	veg.end_year, 
-	vg.GDP,
-	(avg_wages - avg_end_wages)/avg_end_wages * 100 AS yearonyear_increase_wages,
-	(avg_price - avg_end_price)/avg_end_price * 100 AS yearonyear_increase_price,
+	vay.payroll_year,
+	(avg_wages_year - avg_wages_end_year)/avg_wages_end_year * 100 AS yearonyear_increase_wages,
+	(avg_price_year - avg_price_end_year)/avg_price_end_year * 100 AS yearonyear_increase_price,
 	(GDP - end_gdp)/end_gdp * 100 AS yearonyear_increase_gdp
-FROM v_average_price vap  
-JOIN v_average_end_price vaep 
-	ON vaep.end_year = vap.payroll_year 
-JOIN v_average_wages vaw 
-	ON vaw.payroll_year = vap.payroll_year 
-JOIN v_average_end_wages vaew 
-	ON vaew.end_year = vap.payroll_year 
+FROM v_average_year vay 
+JOIN v_average_end_year vaey 
+	ON vaey.end_year = vay.payroll_year 
 JOIN v_gdp vg 
-	ON vg.`year` = vap.payroll_year 
+	ON vg.`year` = vay.payroll_year 
 JOIN v_end_gdp veg 
-	ON veg.end_year = vap.payroll_year ;
+	ON veg.end_year = vay.payroll_year;
+
 	
 SELECT 
 	(GDP - end_gdp)/end_gdp * 100 AS yearonyear_increase_gdp
